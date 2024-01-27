253b672f6f4b258611eeccedf9caaa747b12966c
alexli2024@outlook.com <alexli2024@outlook.com>
2024-01-27 20:59:25 +0300

    Json read

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+using Utf8Json;
 using System.Windows.Forms;
 
 namespace test
@@ -15,6 +11,27 @@ namespace test
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string jsonconf = Application.StartupPath.ToString() + "\\updater.conf";
+            if (File.Exists(jsonconf))
+            {
+                string confjson = File.ReadAllText(jsonconf);
+                var datajson = new JavaScriptSerializer().Deserialize<dynamic>(confjson);
+                textBox1.Text = datajson["start_ip"];
+                textBox2.Text = datajson["stop_ip"];
+                checkBox1.Checked = datajson["update_on_match"];
+                checkBox2.Checked = datajson["update_folder"];
+                label1.Text = datajson["streams"].ToString();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
